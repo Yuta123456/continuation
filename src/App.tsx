c3b2235629d780790fac55e3f502606e4b79d4f4
Yuta123456 <yuuta09090530@icloud.com>
2022-05-17 22:23:25 +0900

    それに伴う変更

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
+import { ellipse, home, square, statsChart, triangle } from 'ionicons/icons';
+import Home from './pages/Home';
+import Stats from './pages/Stats';
 import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
@@ -41,11 +41,11 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/home">
+            <Home />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/stats">
+            <Stats />
           </Route>
           <Route path="/tab3">
             <Tab3 />
@@ -55,17 +55,13 @@ const App: React.FC = () => (
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={home} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab="stats" href="/stats">
+            <IonIcon icon={statsChart}/>
+            <IonLabel>Stats</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
