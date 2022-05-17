d8b921b40103d4449612f8697fbbe86075a58205
Yuta123456 <yuuta09090530@icloud.com>
2022-05-18 03:58:49 +0900

    Pixelaを埋め込む

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
-
+import Pixela from '../components/Pixela';
 const Tab2: React.FC = () => {
   return (
     <IonPage>
@@ -15,6 +15,7 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <Pixela/>
       </IonContent>
     </IonPage>
   );
