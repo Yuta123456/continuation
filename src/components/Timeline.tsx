fb9c1d1a04f6bb098795a8b58e8e8b8e8f8a1928
Yuta123456 <yuuta09090530@icloud.com>
2022-05-20 22:40:40 +0900

    無駄なconsole.logを削除

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -3,7 +3,7 @@ import Post from "./Post";
 const Timeline: React.FC = () => {
     return(
         <>
-            {[...Array(5)].map((_, index) => {console.log(index.toString());return <Post key={index} post_id={index.toString()} hey="aaa"></Post>;})}
+            {[...Array(5)].map((_, index) => <Post key={index} post_id={index.toString()} hey="aaa"></Post>)}
         </>
     );
 }
