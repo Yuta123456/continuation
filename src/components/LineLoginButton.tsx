aab0791a76908203367a060ad50fb4fe4d4fd0c0
Yuta123456 <yuuta09090530@icloud.com>
2022-05-30 02:19:02 +0900

    URLを変更

--- src/components/LineLoginButton.tsx
+++ src/components/LineLoginButton.tsx
@@ -4,13 +4,13 @@ import getRandomString from '../util/getRandomString';
 import React from 'react';
 const LineLoginButton: React.FC = () => {
     const lineState = getRandomString();
-    // https://6293a7edbec9cf66e04cb21e--musical-lamington-39fe95.netlify.app/auth
+    // https://musical-lamington-39fe95.netlify.app/auth
     // TODO: ごめんなさい今度治す
     let URL;
     if (process.env.NODE_ENV === "development") {
         URL = "http%3A%2F%2Flocalhost%3A8100%2Fauth";
     } else {
-        URL = "https%3A%2F%2F6293a7edbec9cf66e04cb21e--musical-lamington-39fe95.netlify.app%2Fauth";
+        URL = "https%3A%2F%2Fmusical-lamington-39fe95.netlify.app%2Fauth";
     }
     console.log("https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=" + CONFIG["CHANNEL_ID"]+"&redirect_uri=" + URL + "&state=" + lineState + "&scope=profile%20openid");
     return (
