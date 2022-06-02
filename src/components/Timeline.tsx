477a9356cdb5ccd0c9da583a12bf500848e4f6ec
Yuta123456 <yuuta09090530@icloud.com>
2022-06-02 16:09:49 +0900

    タイムラインを取得

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,9 +1,35 @@
 import Post from "./Post";
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { userInfoContext } from "../userHooks";
+import { ContributionData, fetchData } from "../util/fetchData";
+import contData2TimeLine from "../util/contData2TimeLine";
 const Timeline: React.FC = () => {
+    const ctx = useContext(userInfoContext);
+    const [ContributionData, setContinuationData] = useState<ContributionData>()
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        const _fetch = async () => {
+            const newData = await fetchData(ctx.userInfo?.userId);
+            if (newData !== null) {
+                setContinuationData(newData);
+            }
+        }
+        setIsLoading(true);
+        _fetch().finally(() => setIsLoading(false));
+    }, [ctx.userInfo]);
+    
+    if (!ctx.userInfo) {
+        return (
+            <div>
+                タイムラインを閲覧するためにはログインが必要です
+            </div>
+        );
+    }
+    
+    const timeLine = contData2TimeLine(ContributionData);
     return(
         <>
-            {[...Array(5)].map((_, index) => <Post key={index} post_id={index.toString()} hey="aaa"></Post>)}
+            {timeLine.map((day, index) => <Post key={index} post_id={index.toString()} day={day}></Post>)}
         </>
     );
 }
