f82b010ebde741198fb8981a23198b38958b0940
Yuta123456 <yuuta09090530@icloud.com>
2022-06-05 23:40:42 +0900

    ログインページを少しおしゃれに

--- src/components/LineLoginButton.tsx
+++ src/components/LineLoginButton.tsx
@@ -9,7 +9,7 @@ const LineLoginButton: React.FC = () => {
     const lineState = getRandomString();
     const ctx = useContext(userInfoContext);
     return (
-        <IonButton href={"https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=" + CONFIG["CHANNEL_ID"]+"&redirect_uri=" + redirect_uri + "&state=" + lineState + "&scope=profile%20openid"}>
+        <IonButton fill="clear" href={"https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=" + CONFIG["CHANNEL_ID"]+"&redirect_uri=" + redirect_uri + "&state=" + lineState + "&scope=profile%20openid"}>
             <IonImg src={imgSrc} alt="icon"/>
         </IonButton>
     );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,21 @@
 import LineLoginButton from "../components/LineLoginButton";
 import React, { useContext } from 'react';
-import { IonButtons, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButtons, IonContent, IonHeader, IonImg, IonPage, IonRippleEffect, IonText, IonTitle, IonToolbar } from "@ionic/react";
 import { userInfoContext } from "../userHooks";
+import imgSrc from '../data/login.png';
 const Login: React.FC = () => {
     const ctx = useContext(userInfoContext);
-    console.log(ctx);
     return (
         <IonPage>
-            <IonContent fullscreen>
-            <LineLoginButton/>
-            <IonText class="ion-text-center">
-                <h1>このアプリを使用するにはログインが必要です。</h1>
+            <IonContent fullscreen class="ion-text-center">
+            <IonText style={{fontFamily: "Courier New"}}>
+                <h1>継続Bot</h1>
             </IonText>
+            <IonImg src={imgSrc}/>
+            <IonText>
+                <p>このアプリを使用するにはログインが必要です。</p>
+            </IonText>
+            <LineLoginButton/>
             </IonContent>
         </IonPage>
     );
