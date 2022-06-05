c5f3103a64040756ea30bb05b08033a9b92245b1
Yuta123456 <yuuta09090530@icloud.com>
2022-06-05 23:53:06 +0900

    画像の大きさをいい感じにして中央寄せ

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,19 @@
 import LineLoginButton from "../components/LineLoginButton";
-import React, { useContext } from 'react';
-import { IonButtons, IonContent, IonHeader, IonImg, IonPage, IonRippleEffect, IonText, IonTitle, IonToolbar } from "@ionic/react";
-import { userInfoContext } from "../userHooks";
+import React from 'react';
+import { IonContent, IonImg, IonPage, IonText} from "@ionic/react";
 import imgSrc from '../data/login.png';
 const Login: React.FC = () => {
-    const ctx = useContext(userInfoContext);
     return (
         <IonPage>
             <IonContent fullscreen class="ion-text-center">
-            <IonText style={{fontFamily: "Courier New"}}>
-                <h1>継続Bot</h1>
-            </IonText>
-            <IonImg src={imgSrc}/>
-            <IonText>
-                <p>このアプリを使用するにはログインが必要です。</p>
-            </IonText>
-            <LineLoginButton/>
+                <IonText style={{fontFamily: "Courier New"}}>
+                    <h1>継続Bot</h1>
+                </IonText>
+                <IonImg src={imgSrc} style={{width:"300px", display: "block", margin: "auto"}}/>
+                <IonText>
+                    <p>このアプリを使用するにはログインが必要です。</p>
+                </IonText>
+                <LineLoginButton/>
             </IonContent>
         </IonPage>
     );
