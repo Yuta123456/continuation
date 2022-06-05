16c4e3e7dca92cdbee4faff7c44f881f7246b98e
Yuta123456 <yuuta09090530@icloud.com>
2022-06-05 22:04:58 +0900

    LINEアイコンボタンをいい感じに設定

--- src/components/LineLoginButton.tsx
+++ src/components/LineLoginButton.tsx
@@ -1,20 +1,17 @@
 import CONFIG from '../const/config';
-import imgSrc from '../data/LINE_Login_Button_Image/Line_Login_Button_Image/images/DeskTop/1x/32dp/btn_login_base.png';
+import imgSrc from '../data/LINE_Login_Button_Image/Line_Login_Button_Image/images/DeskTop/2x/20dp/btn_login_base.png';
 import getRandomString from '../util/getRandomString';
 import React from 'react';
 import { redirect_uri } from '../const/url';
+import { IonButton } from '@ionic/react';
 const LineLoginButton: React.FC = () => {
     const lineState = getRandomString();
     // https://musical-lamington-39fe95.netlify.app/auth
     // TODO: ごめんなさい今度治す
     return (
-        <div>
-            <button>
-                <a href={"https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=" + CONFIG["CHANNEL_ID"]+"&redirect_uri=" + redirect_uri + "&state=" + lineState + "&scope=profile%20openid"}>
-                <img src={imgSrc} alt="icon"/>
-                </a>
-            </button>
-        </div>
+        <IonButton href={"https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=" + CONFIG["CHANNEL_ID"]+"&redirect_uri=" + redirect_uri + "&state=" + lineState + "&scope=profile%20openid"}>
+            <img src={imgSrc} alt="icon"/>
+        </IonButton>
     );
 }
 
