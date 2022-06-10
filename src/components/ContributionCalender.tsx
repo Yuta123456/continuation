0d1c18480992af4a8cb3a9f6502e8e10a95b3522
Yuta123456 <yuuta09090530@icloud.com>
2022-06-10 15:06:58 +0900

    contextからデータを取得

--- src/components/ContributionCalender.tsx
+++ src/components/ContributionCalender.tsx
@@ -1,9 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import Calendar from 'react-github-contribution-calendar';
 import { userInfoContext } from '../userHooks';
 import { formatDate } from '../util/dateFomatter';
-import { ContributionData, fetchData } from '../util/fetchData';
-import Loading from './Loading';
 
 const until = formatDate(new Date(), "yyyy-MM-dd");
 const weekNames = ['', 'M', '', 'W', '', 'F', ''];
@@ -18,35 +16,21 @@ const dateFormat = 'YYYY-MM-DD'
 
 const ContributionCalender: React.FC = () => {
   const ctx = useContext(userInfoContext);
-  const [continuationData, setContinuationData] = useState<ContributionData>({}); 
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const _fetch = async () => {
-      const newData = await fetchData(ctx.userInfo?.userId);
-      if (newData !== null) {
-        setContinuationData(newData);
-      }
-    }
-    setIsLoading(true);
-    _fetch().finally(() => setIsLoading(false));
-  }, [ctx.userInfo]);
-
+  const contributionData = ctx.userInfo?.contributionData;
+  if (contributionData === undefined) {
+    return <></>
+  }
   return (
-    <>
-      {
-        isLoading ? <Loading isLoading={isLoading}/> : <Calendar 
-        panelColors={panelColors} 
-        panelAttributes={undefined}  
-        until={until}
-        weekNames={weekNames}
-        weekLabelAttributes={undefined}   
-        monthNames={monthNames}
-        monthLabelAttributes={undefined}
-        values={continuationData} dateFormat={dateFormat}
-        />
-      }
-    </>  
+    <Calendar 
+      panelColors={panelColors} 
+      panelAttributes={undefined}  
+      until={until}
+      weekNames={weekNames}
+      weekLabelAttributes={undefined}   
+      monthNames={monthNames}
+      monthLabelAttributes={undefined}
+      values={contributionData} dateFormat={dateFormat}
+    />
   );
 }
 
