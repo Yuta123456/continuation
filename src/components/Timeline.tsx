9d9e39b8105b2a7da0877810d2ed234c4b755559
Yuta123456 <yuuta09090530@icloud.com>
2022-06-10 15:07:34 +0900

    不必要な記述を削除

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,13 +1,11 @@
 import Post from "./Post";
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { userInfoContext } from "../userHooks";
-import { ContributionData, fetchData } from "../util/fetchData";
 import contData2TimeLine from "../util/contData2TimeLine";
 import { IonList } from "@ionic/react";
 import HelpContent from "./HelpContent";
 const Timeline: React.FC = () => {
     const ctx = useContext(userInfoContext);
-    const [, setIsLoading] = useState(false);
     const timeLine = contData2TimeLine(ctx.userInfo?.contributionData);
 
     if (timeLine.length === 0) {
