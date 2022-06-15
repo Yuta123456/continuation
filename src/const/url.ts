842bc8b07c50ad95bbfe860f16c8eebb82e9a75a
Yuta123456 <yuuta09090530@icloud.com>
2022-06-15 23:38:00 +0900

    URLを変更

--- src/const/url.ts
+++ src/const/url.ts
@@ -2,7 +2,7 @@ export let redirect_uri: string;
 if (process.env.NODE_ENV === "development") {
     redirect_uri = "http%3A%2F%2Flocalhost%3A8100%2Fauth";
 } else {
-    redirect_uri = "https%3A%2F%2Fmusical-lamington-39fe95.netlify.app%2Fauth";
+    redirect_uri = "https%3A%2F%2Fcontinuationapp.netlify.app%2Fauth";
 }
 
 export let api_url: string;
